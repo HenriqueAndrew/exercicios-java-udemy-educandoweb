320654fa09aa93fddb57959243239b66d0af42e1
HenriqueAndrew <henriqueandrew1986_@hotmail.com>
2022-02-07 21:40:54 -0300

    Atualizando o projeto Programação Orientado a Objetos - Estoque

--- src/exercicioProgOrientObjetosEstoque/Produto.java
+++ src/exercicioProgOrientObjetosEstoque/Produto.java
@@ -2,9 +2,45 @@ package exercicioProgOrientObjetosEstoque;
 
 public class Produto {
 
-	public String nome;
-	public double preco;
-	public int quantidade;
+	//Encapsulamento: atributos privados na classe, podem serem acessados ou alterados por outras classes apenas pelos métodos públicos getters e setters
+	private String nome; 
+	private double preco;
+	private int quantidade;
+	
+	public Produto() {
+	}
+	
+	//Construtores diferentes = sobrecarga de construtores
+	public Produto(String nome, double preco, int quantidade) { 
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
 
 	public double totalValorEmEstoque() {
 		return preco * quantidade;
@@ -19,8 +55,12 @@ public class Produto {
 	}
 
 	public String toString() {
-		return "Nome: " + this.nome + "\nPreço Unitário: $ " + String.format("%.2f", this.preco) + "\nQuantidade em estoque: " + quantidade
-				+ " unidades" + "\nValor Total: $ " + String.format("%.2f", this.totalValorEmEstoque());
+		return "Nome: " + this.nome 
+				+ "\nPreço Unitário: $ " 
+				+ String.format("%.2f", this.preco) 
+				+ "\nQuantidade em estoque: " + quantidade
+				+ " unidades" + "\nValor Total: $ " 
+				+ String.format("%.2f", this.totalValorEmEstoque());
 	}
 
 }
--- src/exercicioProgOrientObjetosEstoque/Programa.java
+++ src/exercicioProgOrientObjetosEstoque/Programa.java
@@ -11,37 +11,42 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 
 		int operacao = 0;
-		int quantidade;
-
-		Produto produto = new Produto();
+		int qtAlterar;
+		
 		System.out.println("Digite os dados do produto: ");
 		System.out.print("Nome:");
-		produto.nome = sc.nextLine();
+		String nome = sc.nextLine();
 		System.out.print("Preço");
-		produto.preco = sc.nextDouble();
+		double preco = sc.nextDouble();
 		System.out.print("Quantidade: ");
-		produto.quantidade = sc.nextInt();
+		//int qt = sc.nextInt();
+		Produto produto = new Produto(nome , preco);
 
 		System.out.println("Dados do produto>>> \n" + produto);
 
 		System.out.println(
 				"\n\nInforme a operação que deseja realizar\n" + "1 - Adicionar quantidade do produto no estoque\n"
-						+ "2 - Retirar quantidade do produto no estoque\n" + "0 - Sair do sistema");
+						+ "2 - Retirar quantidade do produto no estoque\n" + /*"3 - Corrigir o nome do Produto\n" +*/  "0 - Sair do sistema");
 
 		operacao = sc.nextInt();
 
 		if (operacao == 1) {
 			System.out.println("\nDigite a quantidade do produto que deseja adicionar em estoque: ");
-			quantidade = sc.nextInt();
-			produto.adicionaProdutos(quantidade);
+			qtAlterar = sc.nextInt();
+			produto.adicionaProdutos(qtAlterar);
 			System.out.println("Dados do produto>>> \n" + produto);
 		} else if (operacao == 2) {
 			System.out.println("\nDigite a quantidade do produto que deseja retirar do estoque: ");
-			quantidade = sc.nextInt();
-			produto.removerProdutos(quantidade);
-
-			System.out.println("Dados do produto>>> \n" + produto);
-		} else {
+			qtAlterar = sc.nextInt();
+			produto.removerProdutos(qtAlterar);
+			System.out.println("Dados do produto>>> \n" + produto);			
+		} /*else if(operacao == 3){
+			System.out.println("\nDigite o novo nome do produto: ");
+			String n = sc.nextLine();
+			produto.setNome(n);
+			System.out.println("Dados do produto>>> \n" + produto);	
+		}*/
+		else {
 			System.out.println("Programa finalizado!");
 		}
 
