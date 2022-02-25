bfa32fcce9b9e3524ef14034c10776942ca69a6d
HenriqueAndrew <henriqueandrew1986_@hotmail.com>
2022-02-24 21:15:19 -0300

    Acrescentando exercicio - Trabalhando com Enumerações

---
+++ src/exercicioEnumeracoes/Programa.java
@@ -0,0 +1,21 @@
+package exercicioEnumeracoes;
+
+import java.util.Date;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pedido pedido = new Pedido(1080, new Date(), StatusPedido.AGUARDANDO_PAGAMENTO);
+		
+		System.out.println(pedido);
+		
+        StatusPedido sp1 = StatusPedido.ENVIADO;
+        StatusPedido sp2 = StatusPedido.valueOf("ENTREGUE");
+        
+        System.out.println(sp1);
+        System.out.println(sp2);
+
+	}
+
+}
---
+++ src/exercicioEnumeracoes/StatusPedido.java
@@ -0,0 +1,10 @@
+package exercicioEnumeracoes;
+
+public enum StatusPedido {
+	
+	AGUARDANDO_PAGAMENTO,
+	PROCESSANDO,
+	ENVIADO,
+	ENTREGUE;
+
+}
